29f1ac1ac964d4324529374aaf2de20c25c9171f
Paulo-21 <paul.cibier@gmail.com>
2024-03-04 22:18:59 +0100

    add

--- afgcnv3_rx.py
+++ afgcnv3_rx.py
@@ -11,6 +11,7 @@ import ctypes
 
 import multiprocessing as mp
 import numpy as np
+import rustworkx as rx
 import networkx as nx
 import time
 import af_reader_py
@@ -20,53 +21,59 @@ from dgl.nn import GraphConv
 af_data_root = "../af_data/"
 
 def graph_coloring(nx_G):
-    coloring = nx.algorithms.coloring.greedy_color(nx_G, strategy='largest_first')
+    coloring = rx.graph_greedy_color(nx_G)
     return coloring
 
-def calculate_node_features(nx_G, hcat, card, noselfatt, maxb, gr):
+def calculate_node_features(nx_G, hcat, card, noselfatt, maxb, gr, eigenvector_centrality):
+    #coloring = graph_coloring(nx_G)
+    #tic = time.perf_counter()
+    #eigenvector_centrality = rx.eigenvector_centrality(nx_G, max_iter=10000)
+    #toc = time.perf_counter()
+    #print("EIGEN_VEC : ", toc-tic)
     tic = time.perf_counter()
-    coloring = graph_coloring(nx_G)
-    print("GREEDY  ", time.perf_counter()-tic)
+    page_rank = rx.pagerank(nx_G)
+    print("PAGERANK : ", time.perf_counter()-tic)
     tic = time.perf_counter()
-    page_rank = nx.pagerank(nx_G)
-    print("PAGE ", time.perf_counter()-tic)
+    #closeness_centrality = rx.closeness_centrality(nx_G)
+    print("CLOSENESS : ", time.perf_counter()-tic)
+    #in_degrees = nx_G.in_degree()
+    #out_degrees = nx_G.out_degree()
     tic = time.perf_counter()
-    closeness_centrality = nx.degree_centrality(nx_G)
-    print("DEGREE ", time.perf_counter()-tic)
-    tic = time.perf_counter()
-    eigenvector_centrality = nx.eigenvector_centrality(nx_G, max_iter=10000)
-    print("EIGEN ", time.perf_counter()-tic)
-    tic = time.perf_counter()
-    in_degrees = nx_G.in_degree()
-    out_degrees = nx_G.out_degree()
-    print("IN OUT ", time.perf_counter()-tic)
-    tic = time.perf_counter()
-    raw_features = []*nx_G.num_nodes()
+    raw_features = {}
     for node in nx_G.nodes():
         raw_features[node] = [
-            coloring[node],
+            #coloring[node],
             page_rank[node],
-            closeness_centrality[node],
+            #closeness_centrality[node],
             eigenvector_centrality[node],
-            in_degrees[node],
-            out_degrees[node],
+            nx_G.in_degree(node),
+            nx_G.out_degree(node),
             hcat[node],
             card[node],
             noselfatt[node],
             maxb[node],
             gr[node]
         ]
-    print("RAW ", time.perf_counter()-tic)
+    print("RAW : ", time.perf_counter()-tic)
     # Normalize the features
     tic = time.perf_counter()
     scaler = StandardScaler()
     nodes = list(nx_G.nodes())
     feature_matrix = scaler.fit_transform([raw_features[node] for node in nodes])
-    print("scalar : ", time.perf_counter()-tic)
+    print("SCAL : ", time.perf_counter()-tic)
+
     # Create the normalized features dictionary
     normalized_features = {node: feature_matrix[i] for i, node in enumerate(nodes)}
 
     return normalized_features
+def standard_feature(raw_features,  nx_G):
+    scaler = StandardScaler()
+    #nodes = list(nx_G.nodes())
+    #feature_matrix = scaler.fit_transform([raw_features[i] for i, node in enumerate(nodes)])
+    feature_matrix = scaler.fit_transform(raw_features)
+    # Create the normalized features dictionary
+    #normalized_features = {node: feature_matrix[i] for i, node in enumerate(nodes)}
+    return feature_matrix
 
 def transfom_to_graph(label_path, n):
     f = open(label_path, 'r')
@@ -82,26 +89,35 @@ def get_item(af_path):
     
     tic = time.perf_counter()
     #att1, att2, nb_el = af_reader_py.reading_cnf_for_dgl(af_path+".af")
-    att1, att2, nb_el, hcat, card, noselfatt, maxb, gr = af_reader_py.reading_cnf_for_dgl_with_semantics(af_path)
+    att1, att2, nb_el = af_reader_py.reading_cnf_for_dgl(af_path)
     toc = time.perf_counter()
-    #print(toc-tic , " seconds for RUST ")
-    
+    print(toc-tic , " seconds for RUST ")
     graph = dgl.graph((torch.tensor(att1),torch.tensor(att2)), device=device)#.to(device)
-    #print("Graph build in ", toc-tic , " sec")
-    features_tensor = torch.Tensor(3).to(device)
-    #if os.path.isfile(af_data_root+"features_tensor/" + "" + af_name+".pt"):
-        #features_tensor = torch.load(af_data_root+"features_tensor/" + "" + af_name+".pt").to(device)
-        #print("loaded in ", toc-tic , " sec")
-    #else:
     nxg = nx.DiGraph()
     nodes = list([s for s in range(0, nb_el)])
-    att = list([([s, att2[i]]) for i, s in enumerate(att1)])
+    att = list([(s, att2[i]) for i, s in enumerate(att1)])
     nxg.add_nodes_from(nodes)
     nxg.add_edges_from(att)
-    features  = calculate_node_features(nxg, hcat, card, noselfatt, maxb, gr)
-    features_tensor = torch.tensor(np.array([features[node] for node in nxg.nodes()]), dtype=torch.float32).to(device)
+    tic = time.perf_counter()
+    tic3 = time.perf_counter()
+    page_rank = list(nx.pagerank(nxg).values())
+    degree_centrality = list(nx.degree_centrality(nxg).values())
+    in_degrees = list( s for (i, s) in nxg.in_degree())
+    out_degrees = list( s for (i, s) in nxg.out_degree())
+    print("LIST", time.perf_counter()-tic3)
+    #out_degrees = nxg.out_degree()
+    fn = [[0,0,0,0,0]]*nxg.number_of_nodes()
+    tic = time.perf_counter()
+    raw_features = af_reader_py.compute_features(af_path, page_rank, degree_centrality, in_degrees, out_degrees, 10000, 0.00001 )
+    print("Python wrappe the function : ", time.perf_counter()-tic, " sec")
+
+
+    #features  = calculate_node_features(nxg, hcat, card, noselfatt, maxb, gr, eig)
+    features = standard_feature(raw_features, nxg)
+    #features_tensor = torch.tensor(np.array([features[node] for node in nxg.nodes()]), dtype=torch.float32).to(device)
+    tic = time.perf_counter()
+    features_tensor = torch.tensor(features, dtype=torch.float32).to(device)
     #    torch.save(features_tensor, af_data_root+"features_tensor/" + "" + af_name+".pt")
-    
     if graph.number_of_nodes() < nb_el:
         graph.add_nodes(nb_el - graph.number_of_nodes())
     
@@ -111,6 +127,7 @@ def get_item(af_path):
     inputs = torch.randn(graph.number_of_nodes(), 128 , dtype=torch.float32, requires_grad=False).to(device)
     inputs_to_overwrite = inputs.narrow(0, 0, num_rows_to_overwrite).narrow(1, 0, num_columns_in_features)
     inputs_to_overwrite.copy_(features_tensor)
+    print("end ", time.perf_counter()-tic)
     return graph, inputs
 
 class GCN(nn.Module):
@@ -143,9 +160,12 @@ class GCN(nn.Module):
 file = sys.argv[1]
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = GCN(128, 128, 128, 1).to(device)
-
+#model = GCN(128, 128, 128, 1).to(device)
+tic = time.perf_counter()
 graph, inputs = get_item(af_path=file)
+print("get item ", time.perf_counter()-tic)
+"""
 with torch.no_grad():
     out = model(graph, inputs)
     predicted = (torch.sigmoid(out.squeeze())>0.9).float()
+"""
--- bench_gcn.py
+++ bench_gcn.py
@@ -7,18 +7,26 @@ from psutil import TimeoutExpired
 dir = "../af_data/dataset_af/"
 max_time = 60
 t1 , t2= 0, 0
+n_timeout1, n_error1, n_timeout2, n_error2 = 0, 0, 0, 0
+
 
 for file in os.listdir(dir):
+    #if file.startswith("adm"):
+        #continue
     path = os.path.join(dir, file)
     print(file)
     try:
         tic = time.perf_counter()
-        code = subprocess.call(["py", "afgcnv3.py", path], timeout=max_time)
+        code = subprocess.call(["py", "afgcnv3_rx.py", path], timeout=max_time)
         toc = time.perf_counter()
         t1 = toc - tic
     except subprocess.TimeoutExpired:
         t1 = "timeout"
-    
+        n_timeout1 +=1
+    if code != 0:
+        t1 = "error"
+        n_error1 +=1
+    """
     try:
         tic2 = time.perf_counter()
         code1 = subprocess.call(["py", "solver_ag.py", "--filepath", path, "--task", "DC-CO", "--argument", "1"], timeout=max_time)
@@ -26,5 +34,9 @@ for file in os.listdir(dir):
         t2 = toc2 - tic2
     except subprocess.TimeoutExpired:
         t2 = "timeout"
-
+        n_timeout2 +=1
+    if code1 != 0:
+        t2 = "error"
+        n_error2 +=1
+    """
     print(t1, " ", t2)
